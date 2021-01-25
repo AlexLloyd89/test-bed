2fecd64e0cab3ef59ef82a3089de82a5911d2994
Alex Lloyd <lloyd.da89@gmail.com>
2021-01-25 09:56:54 -0500

    add material, basic router set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule),
+  },
+  {
+    path: "search",
+    loadChildren: () => import('./modules/search/search.module').then(m => m.SearchModule),
+    // canLoad: [LoginGuard],
+  },
+  {
+    path: "**",
+    loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule),  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
